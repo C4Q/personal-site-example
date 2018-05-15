5aa6f4f96cdbe060e3735ed2d873e2fa722d1657
mthorry <mthorry@gmail.com>
2018-05-15 16:13:55 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Here's our example app!</h1>
+          <h1 className="App-title">Isn't it so cool?!</h1>
+        </header>
+        <p>Go to netlify.com to deploy your personal site!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
